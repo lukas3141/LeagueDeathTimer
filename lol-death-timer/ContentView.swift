de9ee338e14eb167aba7f7a210da35c424a611a2
Tohr01 <carl@kratzundraabe.de>
2024-11-03 20:06:17 +0100

    Initial Commit

---
+++ lol-death-timer/ContentView.swift
@@ -0,0 +1,26 @@
+//
+// ContentView.swift
+// lol-death-timer
+//
+// Created by Tohr01 on 03.11.24
+// Copyright © 2024 Tohr01. All rights reserved.
+//
+        
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ lol-death-timer/lol_death_timerApp.swift
@@ -0,0 +1,19 @@
+//
+// lol_death_timerApp.swift
+// lol-death-timer
+//
+// Created by Tohr01 on 03.11.24
+// Copyright © 2024 Tohr01. All rights reserved.
+//
+        
+
+import SwiftUI
+
+@main
+struct lol_death_timerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
